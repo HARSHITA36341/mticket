5037427895e03e192d6d834b8c3f00a7200f1aa1
Harshita <166109088+HARSHITA36341@users.noreply.github.com>
2025-07-27 09:36:18 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,4 @@
+document.getElementById('buyBtn').addEventListener('click', () => {
+  alert('Redirecting to ticket purchase page...');
+  // You can use window.location.href = "https://example.com";
+});
